19ed854ff08f4ac3f9e3231f1561c56c64276d00
Fengda HUANG <h@phodal.com>
2015-11-19 07:48:58 +0800

    change application name

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ var argv = require('minimist')(process.argv.slice(2));
 var devDeps = Object.keys(require('./package.json').devDependencies);
 
 
-var appName = argv.name || argv.n || 'ElectronReact';
+var appName = argv.name || argv.n || 'Echeveria Editor';
 var shouldUseAsar = argv.asar || argv.a || false;
 var shouldBuildAll = argv.all || false;
 
