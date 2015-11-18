11a75a0da75cc14d65224604b625c6a9c98358a3
Fengda HUANG <h@phodal.com>
2015-11-18 23:27:08 +0800

    Add ui fixed

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.module.css';
-const { TextField,Snackbar } = require('material-ui');
+const { TextField,Snackbar,DatePicker } = require('material-ui');
 const GitHubApi = require("github-api");
 
 
@@ -22,7 +22,7 @@ export default class Home extends Component {
       sending: 1
     });
 
-    if(localStorage.getItem('token') === null || localStorage.getItem('repo') === null){
+    if (localStorage.getItem('token') === null || localStorage.getItem('repo') === null) {
       that.setState({message: "跳转到设置中..."});
       that.refs.snackbar.show();
       return;
@@ -38,7 +38,7 @@ export default class Home extends Component {
     });
     var repo = github.getRepo(username, reponame);
     repo.read('master', 'README.md', function (err, data) {
-      that.setState({message: "上传成功"});
+      that.setState({message: "上传成功" + data});
       that.refs.snackbar.show();
       that.setState({
         sending: 0
@@ -46,7 +46,17 @@ export default class Home extends Component {
     });
   };
 
+  getStyles() {
+    return {
+      link: {
+        marginLeft: "2em"
+      }
+    };
+  };
+
+
   render() {
+    var inlineStyles = this.getStyles();
     var Message = this.state.message;
 
     return (
@@ -57,9 +67,14 @@ export default class Home extends Component {
               <i className="fa fa-fw fa-edit mode"></i>
               <TextField
                 hintText="标题"/>
+              <TextField
+                style={inlineStyles.link}
+                defaultValue=""
+                hintText="链接名,如: biaoti-2014"/>
             </div>
             <div className={styles.publish}>
-              <button type="submit" className="fa fa-fw fa-paper-plane-o mode" onClick={this.handleSubmit} disabled={this.sending}/>
+              <button type="submit" className="fa fa-fw fa-paper-plane-o mode" onClick={this.handleSubmit}
+                      disabled={this.sending}/>
             </div>
           </div>
           <div id="editor" className={styles.editor}>
