4a0074a6caf9c183da2eff99b9d38fecc8904595
Fengda HUANG <h@phodal.com>
2015-11-18 17:45:16 +0800

    update links

--- app/containers/App.development.js
+++ app/containers/App.development.js
@@ -10,6 +10,14 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <div className="header">
+          <ul className="horizontal">
+            <li><a href="#"><i className="fa fa-fw fa-edit mode"></i></a></li>
+            <li><a href="#"><i className="fa fa-fw fa-wechat mode"></i></a></li>
+            <li><a href="#"><i className="fa fa-fw fa-medium mode"></i></a></li>
+            <li><a href="#"><i className="fa fa-fw fa-gears mode"></i></a></li>
+          </ul>
+        </div>
         {this.props.children}
         <DevTools />
       </div>
