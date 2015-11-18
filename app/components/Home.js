d86d5e2f1eb4126122329dd8fc16af72ec169b9c
Fengda HUANG <h@phodal.com>
2015-11-18 15:38:25 +0800

    make homepage works

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.module.css';
 import GithubAPI from 'github-api';
-import AlloyEditor from 'alloyeditor';
 
 export default class Home extends Component {
   render() {
