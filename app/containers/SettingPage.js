5e70417e0f3f21a2ea910551f324b98ac373f765
Fengda HUANG <h@phodal.com>
2015-11-18 21:17:56 +0800

    Add settings pages

--- app/containers/SettingPage.js
+++ app/containers/SettingPage.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+const { ClearFix, AppCanvas, TextField } = require('material-ui');
 
 export default class SettingPage extends Component {
+  getStyles() {
+    let styles = {
+      group: {
+        width: '100%',
+        float: 'left',
+        marginLeft: 32,
+        marginBottom: 32
+      },
+      textfield: {
+        display: "block",
+        marginTop: 18
+      }
+    };
+    return styles;
+  };
+
   render() {
+    var styles = this.getStyles();
     return (
-      <div>
-          Setting
-      </div>
+      <ClearFix>
+        <div style={styles.group}>
+          <TextField
+            style={styles.textfield}
+            hintText="Hint Text"
+            defaultValue="phodal-archive/echeveria-editor"
+            floatingLabelText="Github Repo Name"/>
+
+          <TextField
+            style={styles.textfield}
+            hintText="Hint Text"
+            defaultValue="xxx"
+            floatingLabelText="Github Token"/>
+        </div>
+      </ClearFix>
     );
   }
 }
