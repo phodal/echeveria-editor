af2b5632132ee3568ffd42368bf538d7df3b6055
Fengda HUANG <h@phodal.com>
2015-11-18 15:33:05 +0800

    add github api and AlloyEditor to home

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.module.css';
-
+import GithubAPI from 'github-api';
+import AlloyEditor from 'alloyeditor';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <div className={styles.container}>
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+        <div className="article">
+          <div className="titleLine">
+            <div id="headline">
+              <i className="fa fa-fw fa-edit mode"></i>
+              <input type="text" id="title" placeholder="标题"/>
+            </div>
+            <div>
+              <button id="publish" type="submit" className="fa fa-fw fa-paper-plane-o mode"></button>
+            </div>
+          </div>
+          <div id="editor">
+            说说你的故事...
+          </div>
         </div>
       </div>
     );
