6e863f838c206daddc773c9dd94dfccebefa2feb
Fengda HUANG <h@phodal.com>
2015-11-18 21:43:40 +0800

    add event for settings page

--- app/containers/SettingPage.js
+++ app/containers/SettingPage.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
-const { ClearFix, AppCanvas, TextField } = require('material-ui');
+const { ClearFix, Snackbar, RaisedButton, TextField } = require('material-ui');
 
 export default class SettingPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      autoHideDuration: 0
+    };
+    this.handleRepoChange = this.handleRepoChange.bind(this);
+    this.handleTokenChange = this.handleTokenChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleRepoChange(e) {
+    this.setState({
+      repo: e.target.value
+    });
+  };
+
+  handleTokenChange(e) {
+    this.setState({
+      token: e.target.value
+    });
+  };
+
+
+  handleSubmit() {
+    this.refs.snackbar.show();
+  };
+
   getStyles() {
     let styles = {
       group: {
@@ -20,20 +47,31 @@ export default class SettingPage extends Component {
 
   render() {
     var styles = this.getStyles();
+    console.log(this.state);
+
     return (
       <ClearFix>
-        <div style={styles.group}>
+        <div style={styles.group} >
           <TextField
             style={styles.textfield}
             hintText="Hint Text"
             defaultValue="phodal-archive/echeveria-editor"
-            floatingLabelText="Github Repo Name"/>
+            floatingLabelText="Github Repo Name"
+            onChange={this.handleRepoChange}/>
 
           <TextField
             style={styles.textfield}
             hintText="Hint Text"
             defaultValue="xxx"
-            floatingLabelText="Github Token"/>
+            floatingLabelText="Github Token"
+            onChange={this.handleTokenChange} />
+
+          <RaisedButton label="Save" secondary={true} onMouseDown={this.handleSubmit}/>
+          <Snackbar
+            ref="snackbar"
+            message="Save Successful"
+            autoHideDuration={this.state.autoHideDuration} />
+
         </div>
       </ClearFix>
     );
