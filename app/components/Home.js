a589cc6999caf73279ad3fed730f25aedee8b2c9
Fengda HUANG <h@phodal.com>
2015-11-18 22:16:55 +0800

    add sumbit events

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.module.css';
-const { TextField } = require('material-ui');
+const { TextField,Snackbar } = require('material-ui');
+const GitHubApi = require("github-api");
+
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      autoHideDuration: 0
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit() {
+    var that = this;
+    var github = new GitHubApi({
+      token: "",
+      auth: "oauth"
+    });
+    var repo = github.getRepo("phodal-archive", "echeveria-content");
+    repo.read('master', 'README.md', function (err, data) {
+      that.refs.snackbar.show();
+      console.log(data);
+    });
+  };
+
   render() {
     return (
       <div>
@@ -12,16 +35,20 @@ export default class Home extends Component {
             <div className={styles.headLine}>
               <i className="fa fa-fw fa-edit mode"></i>
               <TextField
-                hintText="标题" />
+                hintText="标题"/>
             </div>
             <div className={styles.publish}>
-              <button type="submit" className="fa fa-fw fa-paper-plane-o mode"/>
+              <button type="submit" className="fa fa-fw fa-paper-plane-o mode" onClick={this.handleSubmit}/>
             </div>
           </div>
           <div id="editor" className={styles.editor}>
             说说你的故事...
           </div>
         </div>
+        <Snackbar
+          ref="snackbar"
+          message="上传成功"
+          autoHideDuration={this.state.autoHideDuration}/>
       </div>
     );
   }
