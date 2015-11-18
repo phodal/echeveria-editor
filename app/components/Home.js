27a7f7b2633cda57c00a2b8ad02743ee0d41c2ab
Fengda HUANG <h@phodal.com>
2015-11-18 16:36:14 +0800

    import material ui

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.module.css';
 import GithubAPI from 'github-api';
+import TextField from 'material-ui/lib/text-field';
 
 export default class Home extends Component {
   render() {
@@ -11,13 +12,14 @@ export default class Home extends Component {
           <div className={styles.titleLine}>
             <div className={styles.headLine}>
               <i className="fa fa-fw fa-edit mode"></i>
-              <input type="text" id="title" placeholder="标题"/>
+              <TextField
+                hintText="标题" />
             </div>
             <div className={styles.publish}>
               <button type="submit" className="fa fa-fw fa-paper-plane-o mode"/>
             </div>
           </div>
-          <div id="editor">
+          <div id="editor" className={styles.editor}>
             说说你的故事...
           </div>
         </div>
