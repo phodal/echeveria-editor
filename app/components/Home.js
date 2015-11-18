5b93672d0fd3aabf19d9759004e1472bce6b0edd
Fengda HUANG <h@phodal.com>
2015-11-18 16:58:59 +0800

    remove github api form npm packages

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.module.css';
-import GithubAPI from 'github-api';
 const { TextField } = require('material-ui');
 
 export default class Home extends Component {
