158ce5266b5ce76ca5ccb8893a15d5298a757a58
Fengda HUANG <h@phodal.com>
2015-11-19 17:38:09 +0800

    Add for dymanic content

--- app/components/Wechat.js
+++ app/components/Wechat.js
@@ -1,14 +1,41 @@
 import React, { Component, PropTypes } from 'react';
 var ReactQuill = require('react-quill');
+var marked = require('marked');
 
 class Wechat extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      author: "",
+      url: "",
+      date: "",
+      content: "",
+      htmlContent: ""
+    };
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem('data') !== null) {
+      var data = JSON.parse(localStorage.getItem('data'));
+      this.setState({
+        title: data.title,
+        author: data.author,
+        url: data.url,
+        date: data.date,
+        content: data.blogpost,
+        htmlContent: marked(data.blogpost)
+      });
+      console.log(this.state.htmlContent)
+    }
+  }
 
   render() {
     return (
       <div>
         <ReactQuill
           theme="snow"
-          value="{}"/>
+          value={this.state.htmlContent} />
       </div>
     );
   }
